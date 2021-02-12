402e59ec0002a464f8be75af2021ba8b3fe2dd01
01fc <javier.francisco01.09@gmail.com>
2021-02-11 22:28:32 -0600

    initial commit

---
+++ lib/repository/books_repository.dart
@@ -0,0 +1,9 @@
+import 'package:google_books_api/models/book_case.dart';
+
+class BooksRepository {
+  Future<BookCase> getAvailableBooks(String query) async {
+    // https://www.googleapis.com/books/v1/volumes?q=query
+    // TODO: crear modelos antes
+    // TODO: completar request y deserializacion
+  }
+}
